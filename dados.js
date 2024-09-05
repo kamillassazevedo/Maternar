d5f7e43c47912327354679350aa4ebf69d5a6bca
Kamilla <kamilla@MacBook-Air-de-Marcos.local>
2024-09-04 23:33:27 -0300

    terceira aula da imersão

---
+++ dados.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+        title: "Educação Perinatal",
+        description: "A educação perinatal é um processo fundamental para preparar as futuras mães e seus parceiros para a chegada do bebê. Ela abrange um conjunto de conhecimentos e práticas que visam informar e capacitar as famílias sobre a gestação, o parto e o pós-parto.",
+        link: "https://inepes.com.br/",
+    },
+    {
+        title: "Atividade Física",
+        description: "A prática regular de exercícios físicos traz inúmeros benefícios para a saúde, como melhora da condição cardiovascular, fortalecimento muscular e bem-estar mental.",
+        link: "https://www.minhavida.com.br/saude/temas/exercicio-fisico",
+    },
+
+    {
+        title: "Alimentação Saudável",
+        description: "Uma alimentação equilibrada é fundamental para prevenir doenças e promover a saúde. Inclua frutas, legumes, cereais integrais e proteínas magras em sua dieta.",
+        link: "https://www.saude.gov.br/saude-de-a-a-z/alimentacao-saudavel",
+    },
+
+    {
+        title: "Espiritualidade",
+        description: "A espiritualidade pode trazer paz interior, propósito de vida e fortalecer os relacionamentos. Explore diferentes práticas como meditação, yoga e conexão com a natureza.",
+        link: "https://www.portaldobem.com/"
+    },
+
+];
+
+
